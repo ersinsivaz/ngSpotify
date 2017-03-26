4b0f6e3b091b9ae56236dadeee5bb5cb9cf62a9d
ersinsivaz <ersinsivaz@hotmail.com>
2017-03-27 00:00:26 +0300

    firebase facebook login ve username-password login desteği eklendi

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,6 +10,9 @@ import {AngularFire,AuthProviders,AuthMethods} from 'angularfire2';
 export class LoginComponent implements OnInit {
   displayName:string;
   photoUrl:string;
+  userName:string;
+  password:string;
+  loggedIn:boolean;
 
   constructor(private af:AngularFire){
     console.log(af);
@@ -19,11 +22,14 @@ export class LoginComponent implements OnInit {
     this.af.auth.subscribe(authState=>{
       if(!authState){
         console.log("Not logged in");
+        this.loggedIn=false;
       } 
       else {
         console.log("Logged in",authState);
         this.displayName=authState.auth.displayName;
         this.photoUrl=authState.auth.photoURL;
+        this.userName=authState.auth.email;
+        this.loggedIn=true;
       }
     });
   }//.ngOnInit
@@ -31,16 +37,50 @@ export class LoginComponent implements OnInit {
   loginFacebook(){
     this.af.auth.login({
       provider:AuthProviders.Facebook,
-      method:AuthMethods.Popup
-    }).then(authState=>{
-      console.log("After Login",authState);
+      method:AuthMethods.Popup,
+      scope:['public_profile','user_friends']
+    }).then((authState:any)=>{
+      this.af.database.object('/users/'+authState.uid).update({
+        accessToken:authState.facebook.accessToken,
+        name:authState.auth.displayName,
+        photoUrl:authState.auth.photoURL,
+        email:authState.auth.email
+      })
     });
   }//.loginFacebook
 
-  loguotFacebook(){
+  logaout(){
     this.af.auth.logout();
     this.displayName=null;
     this.photoUrl=null;
-  }//.loguotFacebook
+    this.userName=null;
+    this.loggedIn=false;
+  }//.logaoutFacebook
+
+  register(){
+    this.af.auth.createUser({
+      email:'ersinsivaz@gmail.com',
+      password:'12345ss'
+    })
+    .then(authState=>{
+      authState.auth.sendEmailVerification();
+    })
+    .catch(error=>console.log("REGISTER-ERROR",error));
+  }//.register
+
+  login(){
+    this.af.auth.login({
+      email:this.userName,
+      password:this.password
+    },{
+      method:AuthMethods.Password,
+      provider:AuthProviders.Password
+    })
+    .then(authState=>{
+      this.loggedIn=true;
+      console.log("LOGGED-IN-THEN",authState);
+    })
+    .catch(error=>console.log("LOGIN-ERROR",error));
+  }//.login
 
 }
